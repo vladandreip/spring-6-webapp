d822f0791ef2d7b1f7d1dbf31b8453ded4f8dcaf
Popescu Vlad <vlad.a.popescu@softvision.com>
2023-08-14 14:42:27 +0300

    Persist a couple of authors and books

---
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring6webapp.bootstrap;
+
+import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+/**
+ * Component annotation declares this as a Spring component
+ * Spring creates a Spring bean for it
+ * Spring Data JPA is giving the implementation for the repos
+ * H2 memory database sets the ID to the saved entities
+ */
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    //Spring starts -> wires the repositories to the implementations provided by Spring Data JPA.
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author julesVerne = new Author();
+        julesVerne.setFirstName("Jules");
+        julesVerne.setLastName("Verne");
+        Author julesVerneSaved = authorRepository.save(julesVerne);
+
+        Book misteryIslandBook = new Book();
+        misteryIslandBook.setTitle("Mistery Island");
+        misteryIslandBook.setIsbn("123456");
+        Book misteryIslandBookSaved = bookRepository.save(misteryIslandBook);
+
+        Author steinbeck = new Author();
+        steinbeck.setFirstName("Steinbeck");
+        steinbeck.setLastName("John");
+        Author steinbeckSaved = authorRepository.save(steinbeck);
+
+        Book westOfEden = new Book();
+        westOfEden.setTitle("West Of Eden");
+        westOfEden.setIsbn("123123");
+        Book westOfEdenSaved = bookRepository.save(westOfEden);
+        julesVerneSaved.getBooks().add(misteryIslandBookSaved);
+        steinbeckSaved.getBooks().add(westOfEdenSaved);
+
+        authorRepository.save(steinbeckSaved);
+        authorRepository.save(julesVerneSaved);
+
+        System.out.println("In bootstrap");
+        System.out.println("Author count:" + authorRepository.count());
+        System.out.println("Books count:" + bookRepository.count());
+    }
+}
